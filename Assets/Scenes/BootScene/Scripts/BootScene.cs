407223a367ae62d744d721cdae7dc90dda2e10ed
t-hirasawa <takemi.hirasawa@gnaefir.email>
2017-04-05 15:01:08 +0900

    BootScene push

---
+++ Assets/Scenes/BootScene/Scripts/BootScene.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Cosmos.SceneManager;
+
+public class BootScene : MonoBehaviour
+{
+
+	/// <summary>シーンマネージャオブジェクト</summary>///
+	[SerializeField]
+	private GameObject _startUp;
+
+	// Use this for initialization
+	void Start ()
+	{
+		DontDestroyOnLoad (_startUp);
+		AppSceneManager.Instance.LoadScene ("TitleScene", false);
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+}
